02106077f3d7fdf1ec766e282ab6af57ddbe2a7b
Seline Bowens <nytokumu@gmail.com>
2025-04-11 21:38:18 -0400

    Initial commit of Spring Boot application

---
+++ src/main/java/com/gcu/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.gcu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/users/**").authenticated()
+                .anyRequest().permitAll())
+            .formLogin(login -> login
+                .loginPage("/login")
+                .defaultSuccessUrl("/users", true)
+                .permitAll())
+            .logout(logout -> logout.logoutSuccessUrl("/").permitAll());
+
+        return http.build();
+}
+	 @Bean
+	    public InMemoryUserDetailsManager userDetailsService() {
+	        UserDetails user = User.builder()
+	                .username("admin")
+	                .passwordEncoder(passwordEncoder()::encode)
+	                .password("password")
+	                .roles("USER")
+	                .build();
+	        return new InMemoryUserDetailsManager(user);
+	    }
+}
--- src/main/java/com/gcu/controller/UserController.java
+++ src/main/java/com/gcu/controller/UserController.java
@@ -3,6 +3,7 @@ package com.gcu.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.gcu.model.User;
 import com.gcu.service.UserService;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
 	
